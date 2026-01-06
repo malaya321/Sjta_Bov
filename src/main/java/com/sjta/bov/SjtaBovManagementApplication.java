b3770c28ab4db95cb8aca2870a72644eed96f7fd
lingaraj <lingaraj@suyogindia.com>
2026-01-06 16:47:09 +0530

    Initial commit: SJTA BOV Management backend structure

---
+++ src/main/java/com/sjta/bov/SjtaBovManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sjta.bov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjtaBovManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjtaBovManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sjta/bov/SjtaBovManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjta.bov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjtaBovManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
